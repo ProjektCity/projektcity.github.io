a37f20f110f27ee3e38f5cd067d1ef71ba2cfd51
itsmarian <100617987+itsmarianmc@users.noreply.github.com>
2025-02-09 00:06:13 +0100

    modified:   download.html new file:   download/v1.0.html new file:   download/v1.1.html new file:   download/v2.0.html new file:   download/v3.0.html modified:   download/v4.1.html modified:   download/v4.2.html modified:   download/v5.0.html new file:   download/versions.js

---
+++ download/versions.js
@@ -0,0 +1,31 @@
+document.getElementById('redirect-v5-0').addEventListener('click', function() {
+    window.open("/download/v5.0", "_parent");
+});
+
+document.getElementById('redirect-v4-2').addEventListener('click', function() {
+    window.open("/download/v4.2", "_parent");
+});
+
+document.getElementById('redirect-v4-1').addEventListener('click', function() {
+    window.open("/download/v4.1", "_parent");
+});
+
+document.getElementById('redirect-v4-0').addEventListener('click', function() {
+    window.open("/download/v4.0", "_parent");
+});
+
+document.getElementById('redirect-v3-0').addEventListener('click', function() {
+    window.open("/download/v3.0", "_parent");
+});
+
+document.getElementById('redirect-v2-0').addEventListener('click', function() {
+    window.open("/download/v2.0", "_parent");
+});
+
+document.getElementById('redirect-v1-1').addEventListener('click', function() {
+    window.open("/download/v1.1", "_parent");
+});
+
+document.getElementById('redirect-v1-0').addEventListener('click', function() {
+    window.open("/download/v1.0", "_parent");
+});
