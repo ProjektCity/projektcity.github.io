4df1197486f84608566b6424490735773d5ffe2d
itsmarian <100617987+itsmarianmc@users.noreply.github.com>
2025-02-09 17:40:23 +0100

    modified:   download/v1.0.html modified:   download/versions.js

--- download/versions.js
+++ download/versions.js
@@ -1,31 +1,31 @@
 document.getElementById('redirect-v5-0').addEventListener('click', function() {
-    window.open("/download/v5.0", "_parent");
+    window.open("/download/v5.0?utm_src=projektcity_downloadbtn_versiondownload&version=5.0&version_id=v5-0&return_url=https%3A%2F%2Fprojektcity.github.com&2Fdownload", "_parent");
 });
 
 document.getElementById('redirect-v4-2').addEventListener('click', function() {
-    window.open("/download/v4.2", "_parent");
+    window.open("/download/v4.2?utm_src=projektcity_downloadbtn_versiondownload&version=4.2&version_id=v4-2&return_url=https%3A%2F%2Fprojektcity.github.com&2Fdownload", "_parent");
 });
 
 document.getElementById('redirect-v4-1').addEventListener('click', function() {
-    window.open("/download/v4.1", "_parent");
+    window.open("/download/v4.1?utm_src=projektcity_downloadbtn_versiondownload&version=4.1&version_id=v4-1&return_url=https%3A%2F%2Fprojektcity.github.com&2Fdownload", "_parent");
 });
 
 document.getElementById('redirect-v4-0').addEventListener('click', function() {
-    window.open("/download/v4.0", "_parent");
+    window.open("/download/v4.0?utm_src=projektcity_downloadbtn_versiondownload&version=4.0&version_id=v4-0&return_url=https%3A%2F%2Fprojektcity.github.com&2Fdownload", "_parent");
 });
 
 document.getElementById('redirect-v3-0').addEventListener('click', function() {
-    window.open("/download/v3.0", "_parent");
+    window.open("/download/v3.0?utm_src=projektcity_downloadbtn_versiondownload&version=3.0&version_id=v3-0&return_url=https%3A%2F%2Fprojektcity.github.com&2Fdownload", "_parent");
 });
 
 document.getElementById('redirect-v2-0').addEventListener('click', function() {
-    window.open("/download/v2.0", "_parent");
+    window.open("/download/v2.0?utm_src=projektcity_downloadbtn_versiondownload&version=2.0&version_id=v2-0&return_url=https%3A%2F%2Fprojektcity.github.com&2Fdownload", "_parent");
 });
 
 document.getElementById('redirect-v1-1').addEventListener('click', function() {
-    window.open("/download/v1.1", "_parent");
+    window.open("/download/v1.1?utm_src=projektcity_downloadbtn_versiondownload&version=1.1&version_id=v1-1&return_url=https%3A%2F%2Fprojektcity.github.com&2Fdownload", "_parent");
 });
 
 document.getElementById('redirect-v1-0').addEventListener('click', function() {
-    window.open("/download/v1.0", "_parent");
+    window.open("/download/v1.0?utm_src=projektcity_downloadbtn_versiondownload&version=1.0&version_id=v1-0&return_url=https%3A%2F%2Fprojektcity.github.com&2Fdownload", "_parent");
 });
