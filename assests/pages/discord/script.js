15e8967c6713dfbb22602e84dc030ad0266f2621
itsmarian <100617987+itsmarianmc@users.noreply.github.com>
2025-02-09 18:36:44 +0100

    new file:   assests/pages/discord/script.js new file:   assests/pages/discord/style.css modified:   assests/pages/mainpage/style.css modified:   discord.html modified:   download.html modified:   index.html modified:   v5-0.html

---
+++ assests/pages/discord/script.js
@@ -0,0 +1,3 @@
+document.querySelector('.dsc-invitejoiner').addEventListener('click', function() {
+    window.open('https://discord.gg/HeRwHB2QQk?utm_src=projektcity_dsc_joiner&utm_medium=projektcity&return_url=https%3A%2F%2Fprojektcity.github.io%2Fdiscord', '_blank');
+});
