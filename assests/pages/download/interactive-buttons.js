bd04e464e1bcfc672a8439437a888f44ba16dead
itsmarian <marian.duettra@gmail.com>
2025-02-21 21:27:22 +0100

    new file:   assests/pages/download/interactive-buttons.js modified:   download/v1.0.html modified:   download/v1.1.html modified:   download/v2.0.html modified:   download/v3.0.html modified:   download/v4.0.html modified:   download/v4.1.html modified:   download/v4.2.html modified:   download/v5.0.html

---
+++ assests/pages/download/interactive-buttons.js
@@ -0,0 +1,31 @@
+document.getElementById("download-v5-0").addEventListener('click', function() {
+    window.open("https://www.curseforge.com/minecraft/worlds/projekt-city/download/6043384", "_blank")
+});
+
+document.getElementById("download-v4-2").addEventListener('click', function() {
+    window.open("https://www.curseforge.com/minecraft/worlds/projekt-city/download/4832448", "_blank")
+});
+
+document.getElementById("download-v4-1").addEventListener('click', function() {
+    window.open("https://www.curseforge.com/minecraft/worlds/projekt-city/download/4446473", "_blank")
+});
+
+document.getElementById("download-v4-0").addEventListener('click', function() {
+    window.open("https://www.curseforge.com/minecraft/worlds/projekt-city/download/4381533", "_blank")
+});
+
+document.getElementById("download-v3-0").addEventListener('click', function() {
+    window.open("https://www.curseforge.com/minecraft/worlds/projekt-city/download/4326053", "_blank")
+});
+
+document.getElementById("download-v2-0").addEventListener('click', function() {
+    window.open("https://www.curseforge.com/minecraft/worlds/projekt-city/download/4283975", "_blank")
+});
+
+document.getElementById("download-v1-1").addEventListener('click', function() {
+    window.open("https://www.curseforge.com/minecraft/worlds/projekt-city/download/4276853", "_blank")
+});
+
+document.getElementById("download-v1-0").addEventListener('click', function() {
+    window.open("https://www.curseforge.com/minecraft/worlds/projekt-city/download/4276516", "_blank")
+});
