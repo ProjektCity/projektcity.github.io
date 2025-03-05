ffeab1a82ddf3353556a0c0879473ed6dafcbc25
itsmarian <marian.duettra@gmail.com>
2025-03-05 18:56:01 +0100

    new file:   assests/pages/mainpage/meta-tags.js modified:   download/index.html modified:   index.html

---
+++ assests/pages/mainpage/meta-tags.js
@@ -0,0 +1,25 @@
+document.write(`
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Projekt City Team">
+    <meta name="description" content="Projekt City ist eine detaillierte Minecraft-Welt mit einer großen Stadt, einem Flughafen, Hotels und mehr. Erkunde eine riesige, realistische Umgebung!">
+    <meta name="keywords" content="Minecraft, Stadt, City, Map, Download, Projekt City, Gebäude, Flughafen, Redstone, Command Blocks">
+    <meta name="robots" content="index, follow">
+  
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src 'self' https://www.curseforge.com; script-src 'self'">
+    <meta name="referrer" content="no-referrer">
+  
+    <meta property="og:title" content="Projekt City – Die ultimative Minecraft-Stadt">
+    <meta property="og:description" content="Erkunde eine riesige Minecraft-Stadt mit Flughäfen, Hotels, U-Bahn-System und vielem mehr!">
+    <meta property="og:image" content="https://www.curseforge.com/minecraft/worlds/projekt-city/logo.png">
+    <meta property="og:url" content="https://www.curseforge.com/minecraft/worlds/projekt-city">
+    <meta property="og:type" content="website">
+  
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Projekt City – Die ultimative Minecraft-Stadt">
+    <meta name="twitter:description" content="Erkunde eine riesige Minecraft-Stadt mit Flughäfen, Hotels, U-Bahn-System und mehr!">
+    <meta name="twitter:image" content="https://www.curseforge.com/minecraft/worlds/projekt-city/logo.png">
+    <meta name="twitter:site" content="@DeinTwitterHandle">
+  `);
+  
