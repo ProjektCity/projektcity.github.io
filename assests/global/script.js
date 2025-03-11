b8a2b67dc1328dedb589500ae7534eac162634bc
itsmarian <marian.duettra@gmail.com>
2025-03-11 23:09:21 +0100

    Added information about the script formats for better understanding.
    Affected file:
    modified:   assests/global/script.js

--- assests/global/script.js
+++ assests/global/script.js
@@ -7,6 +7,12 @@
     * For more information, please contact us at: projektcityofficial@gmail.com
     * 
     * Script version: 1.1
+    * 
+    * Script format: W.X.Y.Z
+    * W = Finished code, contains no errors, works perfectly
+    * X = Finished code, might contains a few problems, which do not affect tge user experience, bug improvements from W version
+    * Y = Experimental code, contains errors, can limit user experience and make website unusable
+    * Z = Test and experimental code, probably contains bugs that limit the user experience or make the service unusable, use with caution
 */
 
 // Custom CSS styles based on DIV properties
