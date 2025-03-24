8fe3940857a7cb48fdedbf5bb6a04b3ed0d88719
itsmarian <marian.duettra@gmail.com>
2025-03-24 01:35:14 +0100

    Fixed margin issue on download pages
    Affected files:
    - assests/global/script.js
    - download/v1.0.html
    - download/v1.1.html
    - download/v2.0.html
    - download/v3.0.html
    - download/v4.0.html
    - download/v4.1.html
    - download/v4.2.html

--- assests/global/script.js
+++ assests/global/script.js
@@ -127,7 +127,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const deviceType = getDeviceType();
     const browser = getBrowser();
     
-    console.log("[BUILD INFO] Release Channel: stable, Build Number: #582, Update date: 03/23/2025");
+    console.log("[BUILD INFO] Release Channel: stable, Build Number: #583, Update date: 03/23/2025");
     setTimeout(() => {
         console.log(`get-userdevice=${deviceType}`);
         console.log(`get-userbrowser=${browser}`);
