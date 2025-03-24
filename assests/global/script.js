af2cb56613502c6689bc375998a8f32515b69d5c
itsmarian <marian.duettra@gmail.com>
2025-03-24 01:15:39 +0100

    Added German version to the /discord and /download page
    Affected files:
    - assests/lang/discord/index.js
    - assests/lang/download/index.js
    - assests/lang/index.js
    - assests/pages/discord/style.css
    - discord/index.html
    - download/index.html

--- assests/global/script.js
+++ assests/global/script.js
@@ -127,7 +127,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const deviceType = getDeviceType();
     const browser = getBrowser();
     
-    console.log("[BUILD INFO] Release Channel: stable, Build Number: #580, Update date: 03/23/2025");
+    console.log("[BUILD INFO] Release Channel: stable, Build Number: #581, Update date: 03/23/2025");
     setTimeout(() => {
         console.log(`get-userdevice=${deviceType}`);
         console.log(`get-userbrowser=${browser}`);
