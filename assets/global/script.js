e6f5035a66bfc520c2b50a93f5cbf394695742f2
itsmarian <marian.duettra@gmail.com>
2025-04-04 23:59:02 +0200

    Added image tilt to the head image
    Affected files:
    - assets/global/header/navbar-style.css
    - assets/global/style.css
    - index.html

--- assets/global/script.js
+++ assets/global/script.js
@@ -128,7 +128,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const deviceType = getDeviceType();
     const browser = getBrowser();
     
-    console.log("[BUILD INFO] Release Channel: stable, Build Number: #615, Update date: 04/04/2025");
+    console.log("[BUILD INFO] Release Channel: stable, Build Number: #616, Update date: 04/04/2025");
     setTimeout(() => {
         console.log(`get-userdevice=${deviceType}`);
         console.log(`get-userbrowser=${browser}`);
