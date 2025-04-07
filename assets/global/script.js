daabe274284205dcacb75664176e02b3484fbc4c
itsmarian <marian.duettra@gmail.com>
2025-04-07 10:44:24 +0200

    Fixed an issue where the join Discord Server button was not in the center of the page.
    Affected file:
    - assets/pages/discord/style.css

--- assets/global/script.js
+++ assets/global/script.js
@@ -125,7 +125,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const deviceType = getDeviceType();
     const browser = getBrowser();
     
-    console.log("[BUILD INFO] Release Channel: stable, Build Number: #641, Update date: 04/07/2025");
+    console.log("[BUILD INFO] Release Channel: stable, Build Number: #642, Update date: 04/07/2025");
     setTimeout(() => {
         console.log(`get-userdevice=${deviceType}`);
         console.log(`get-userbrowser=${browser}`);
