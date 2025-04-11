bba4b3831e4ca913b03f04d151497fc460e71ed8
itsmarian <marian.duettra@gmail.com>
2025-04-11 18:03:28 +0200

    modified:   assets/global/header/mobile-menu.css

--- assets/global/script.js
+++ assets/global/script.js
@@ -185,7 +185,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
     
-    console.log("[BUILD INFO] Release Channel: stable, Build Number: #677, Update date: 04/11/2025");
+    console.log("[BUILD INFO] Release Channel: stable, Build Number: #678, Update date: 04/11/2025");
     setTimeout(() => {
         console.log(`[USER] get-userdevice=${deviceType}`);
         console.log(`[USER] get-userbrowser=${browser}`);
