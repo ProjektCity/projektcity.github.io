97f8b31643d0cc6c9300b2a9c8a5bc1f5b847e84
itsmarian <marian.duettra@gmail.com>
2025-04-12 00:47:04 +0200

    Resolved an issue where the cookie banner was not changed when switching languages.
    Affected files:
    - assets/lang/discord/index.js
    - assets/lang/download/index.js
    - assets/lang/download/v5.0.js
    - assets/lang/index.js
    - assets/lang/linkout.js

--- assets/global/script.js
+++ assets/global/script.js
@@ -185,7 +185,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
     
-    console.log("[BUILD INFO] Release Channel: stable, Build Number: #681, Update date: 04/12/2025");
+    console.log("[BUILD INFO] Release Channel: stable, Build Number: #682, Update date: 04/12/2025");
     setTimeout(() => {
         console.log(`[USER] get-userdevice=${deviceType}`);
         console.log(`[USER] get-userbrowser=${browser}`);
