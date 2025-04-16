02ada380749281dbe382284f12dac1c16d1f604e
itsmarian <marian.duettra@gmail.com>
2025-04-17 00:31:13 +0200

    Updated from Python (.py) to an .exe file for the installer.
    Affected files:
    - assets/files/Projekt City Installation Manager.zip
    - assets/global/script.js
    - assets/lang/index.js
    - index.html

--- assets/global/script.js
+++ assets/global/script.js
@@ -232,7 +232,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
     
-    console.log("[BUILD INFO] Release Channel: stable, Build Number: #689, Update date: 04/15/2025");
+    console.log("[BUILD INFO] Release Channel: stable, Build Number: #690, Update date: 04/17/2025");
     setTimeout(() => {
         console.log(`[USER] get-userdevice=${deviceType}`);
         console.log(`[USER] get-userbrowser=${browser}`);
