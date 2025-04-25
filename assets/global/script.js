8b2899f14f5e160ac98b116a527a54dc08479251
itsmarian <marian.duettra@gmail.com>
2025-04-25 21:26:21 +0200

    Fixed a style issue.
    Affected file:
    - assets/pages/download/style.css

--- assets/global/script.js
+++ assets/global/script.js
@@ -232,7 +232,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
     
-    console.log("[BUILD INFO] Release Channel: stable, Build Number: #698, Update date: 04/20/2025");
+    console.log("[BUILD INFO] Release Channel: Stable, Build Number: #802, Update Date: 04/25/2025");
     setTimeout(() => {
         console.log(`[USER] get-userdevice=${deviceType}`);
         console.log(`[USER] get-userbrowser=${browser}`);
